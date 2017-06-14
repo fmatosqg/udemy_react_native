5fee46afeddecbdedd536ac9a65e84ea257f75e4
Fabio Goncalves <fabio.goncalves@imac-fabio.ap.media.global.loc>
2017-06-14 15:47:23 +1000

    Refactor components according to linter, copy pasting data.js

--- app/screens/Details.js
+++ app/screens/Details.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-class Details extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-      </View>);
-  }
-}
+const Details = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>);
+};
 
 export default Details;
