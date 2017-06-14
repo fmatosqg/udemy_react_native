1ddd82e58acb187553ef8e6a9267c3806ec050b6
Fabio Goncalves <fabio.goncalves@imac-fabio.ap.media.global.loc>
2017-06-14 16:38:51 +1000

    Lecture Flatlist

--- app/screens/Contacts.js
+++ app/screens/Contacts.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+
+import { contacts } from '../config/data';
+import colors from '../config/colors';
 
 const Contacts = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Contacts Screen</Text>
-    </View>);
+      <Text>Header</Text>
+      <FlatList
+        style={{ backgroundColor: colors.background }}
+        data={contacts}
+        renderItem={({ item }) => { return <View><Text>hello {item.email}</Text></View>; }}
+        keyExtractor={(item) => { return item.email; }}
+      />
+      <Text>Footer</Text>
+    </View>
+  );
 };
 
 export default Contacts;
